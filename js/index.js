ee37ea4837cbde015d1f2122b6d8b8e0e8adc542
Michael Gaming <compmichaelb90@gmail.com>
2024-09-01 17:05:14 +0400

    Finished main page, decorations and features.

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const BUFFER = 40;
+
+function toggleNavMenu() {
+    const navMenu = document.getElementById("navMenu");
+
+    if (navMenu.style.visibility == "visible") {
+        navMenu.style.opacity = "0%";
+        navMenu.style.visibility = "hidden";
+    } else {
+        navMenu.style.opacity = "100%";
+        navMenu.style.visibility = "visible";
+    }
+};
+
+const navLinks = document.querySelectorAll('div[class^="navLink"]:not([class*="1"])');
+console.log(navLinks);
+navLinks.forEach( (navLink) => {
+    navLink.addEventListener("click", e => {
+        alert("Feature not implemented (ask Michael)");
+    });
+});
+
+function navInstagram(instaURL) {
+    window.location.href = instaURL;
+};
+
+window.addEventListener('scroll', e => {
+    const navBar = document.getElementById("navBar");
+    const scrollHeight = window.scrollY || document.body.scrollTop || 0;
+
+    navBar.style.transform  = 
+        (scrollHeight >= (navBar.offsetHeight+BUFFER))?
+            (navBar.style.transform = `translateY(${-navBar.offsetHeight}px)`):
+            ("none");
+});
